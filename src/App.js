ad404ce22c6cccb16c8e9deb7ec5c9393d762647
cirocki <marcincirocki89@gmail.com>
2021-04-24 12:58:22 +0200

    Add env file with api keys

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { apiLink, apiKey } from "./api/apiData";
 
 function App() {
-  return <div className="App"></div>;
+  async function testApi() {
+    let query = "amelia";
+    const result = await fetch(`${apiLink}?apikey=${apiKey}&s=${query}`);
+    const data = await result.json();
+    console.log(data);
+  }
+  testApi();
+  return (
+    <div className="App">
+      <header>
+        <h1>DARK MOVIE APP</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
